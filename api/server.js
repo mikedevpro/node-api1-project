1eaec31837f4382881046a832a2e422207dd98f3
Michael Nobles <Mnobles33@gmail.com>
2025-02-14 14:59:19 -0700

    good commit

--- api/server.js
+++ api/server.js
@@ -4,15 +4,25 @@ const User = require('./users/model')
 const server = express()
 server.use(express.json())
 
+server
+
 server.delete('/api/users/:id', async (req, res) => {
-  const possibleUser = await User.findById(req.params.id)
-  if (!possibleUser) {
-    res.status(404).json({
-      message: 'The user with the specified ID does not exist',
+  try {
+    const possibleUser = await User.findById(req.params.id)
+    if (!possibleUser) {
+      res.status(404).json({
+        message: 'The user with the specified ID does not exist',
+      })
+    } else {
+      const deletedUser = await User.remove(possibleUser.id)
+      res.status(200).json(deletedUser)
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'error creating user',
+      err: err.message,
+      stack: err.stack,
     })
-  } else {
-    const deletedUser = await User.remove(possibleUser.id)
-    res.status(200).json(deletedUser)
   }
 })
 
